db782d31bd0a3aec12c7a8a0312ae9a54c84cc64
kurniawan86 <90589478+kurniawan86@users.noreply.github.com>
2021-10-16 17:35:30 +0700

    add scatter plotting and label

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as panda
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 class classData:
     __data = []
@@ -16,3 +19,15 @@ class classData:
 
     def viewDataset(self):
         print("DATASET \n", self.__data)
+
+    def plottingScatterDataset(self):
+        fix, ax = plt.subplots()
+        x = self.__data
+        temp = np.array(x)
+        ax.scatter(temp[:, 0], temp[:, 1], marker='o')
+        for i in range(temp.shape[0]):
+           ax.annotate(i+1, (temp[i, 0]+0.1, temp[i, 1]+0.1))
+        plt.title("Scatter Dataset")
+        plt.xlabel(' X ')
+        plt.ylabel(' Y ')
+        plt.show()
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from data import classData
 if __name__ == '__main__':
     data = classData()
     data.viewDataset()
+    data.plottingScatterDataset()
